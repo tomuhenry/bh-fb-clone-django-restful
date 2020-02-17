ac8e7047b4c51d9a0edbda8384a5a4d6ce6877c9
tomuhenry <tomusangehenry02@gmail.com>
2020-02-17 14:47:29 +0300

    deploy facebook clone

--- facebook_clone/settings.py
+++ facebook_clone/settings.py
@@ -106,7 +106,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST')
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 
